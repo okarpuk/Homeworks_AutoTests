3a4df50d66ad0ca3f62423543a226088e482af66
okarpuk <oleg.karpuk.1982@gmail.com>
2023-04-12 20:56:13 +0300

    Add and Substract added

---
+++ HomeWork_1_nUnit/Add.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_1_nUnit
+{
+    [TestFixture]
+    public class Add
+    {
+        [TestCase(7, 8, 15)]
+        [TestCase(2.8, 8.7, 11.5)]
+        [TestCase(-100, -5, -105)]
+        [TestCase(-20, 80, 60)]
+
+        public void AddTest(double a, double b, double expected)
+        {
+            // Arrange
+            var calculation = new Calculator();
+
+            // Act
+            var actual = calculation.Add(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ HomeWork_1_nUnit/Subtract.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_1_nUnit
+{
+    [TestFixture]
+    public class Subtract
+    {
+        [TestCase(22, 10, 12)]
+        [TestCase(10, 32, -22)]
+        [TestCase(3.2, 2.1, 1.1)]
+        [TestCase(-5.5, 2.1, -7.6)]
+
+        public void SubtractTest(double a, double b, double expected)
+        {
+            var calculation = new Calculator();
+
+            double actual = calculation.Subtract(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
