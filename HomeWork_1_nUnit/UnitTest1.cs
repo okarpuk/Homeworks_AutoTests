d9439126d9dfbbaad914badf5b319b606c94f6ba
okarpuk <oleg.karpuk.1982@gmail.com>
2023-04-12 19:13:39 +0300

    Add project files.

---
+++ HomeWork_1_nUnit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace HomeWork_1_nUnit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
