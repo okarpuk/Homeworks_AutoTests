1c6ffcd62afd6ad616cefb5a2a37dce97a2ebe3a
okarpuk <oleg.karpuk.1982@gmail.com>
2023-04-18 10:48:37 +0300

    Inputs test created

---
+++ HomeWork_2_Selenium/InputTest.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using NuGet.Frameworks;
+using OpenQA.Selenium.Support.UI;
+using System.Xml;
+
+namespace HomeWork_2_Selenium
+{
+    [TestFixture]
+    internal class InputTest
+    {
+        WebDriver ChromeDriver { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            ChromeDriver = new ChromeDriver();
+            ChromeDriver.Manage().Window.Maximize();
+            ChromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        }
+
+        [Test]
+        public void Inputs()
+        {
+            ChromeDriver.Navigate().GoToUrl("http://the-internet.herokuapp.com");
+            Thread.Sleep(1000);
+            ChromeDriver.FindElement(By.LinkText("Inputs")).Click();
+            Thread.Sleep(1000);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
